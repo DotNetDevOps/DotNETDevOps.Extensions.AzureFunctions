b777f6a87d2ed75e608531d27e41c019dbd0eda9
Poul Kjeldager Sørensen <pks@s-innovations.net>
2019-05-17 07:35:01 +0200

    added UseAppSettingsJson

---
+++ src/DotNETDevOps.Extensions.AzureFunctions/Extensions/AspNetCoreDefaultConfigurationExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNETDevOps.Extensions.AzureFunctions.Extensions
+{
+    public static class AspNetCoreDefaultConfigurationExtension
+    {
+        public static IWebHostBuilder UseAppSettingsJson(this IWebHostBuilder hostBuilder)
+        {
+            return hostBuilder.ConfigureAppConfiguration((context, configurationBuilder) =>
+            {
+                configurationBuilder
+                    //  .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true);
+
+             
+            });
+        }
+    }
+}
